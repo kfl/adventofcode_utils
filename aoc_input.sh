558c7355b1c5cd3b1dd620289e5c360a85a366c8
Ken Friis Larsen <ken@friislarsen.net>
2023-11-30 10:59:21 +0100

    Read session cookie from file instead of environment variable

--- aoc_input.sh
+++ aoc_input.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
+USER_AGENT="github.com/kfl/adventofcode_utils/blob/main/aoc_input.sh by ken@friislarsen.net"
+
 YEAR=$1
 DAY=$2
 
+CURRENT_DATE=$(date '+%Y-%m-%d')
+
 # If YEAR and DAY are not provided, use the current date
-if [ -z "$YEAR" ] || [ -z "$DAY" ]; then
-    CURRENT_DATE=$(date '+%Y-%m-%d')
+if [ -z "$YEAR" ]; then
     YEAR=$(date -d "$CURRENT_DATE" '+%Y')
+fi
+if [ -z "$DAY" ]; then
     DAY=$(date -d "$CURRENT_DATE" '+%d')
 fi
 
-# Read AOC_SESSION_COOKIE from environment variable
-if [ -z "$AOC_SESSION_COOKIE" ]; then
-    echo "Error: AOC_SESSION_COOKIE environment variable is not set."
+# Read session cookie from file
+COOKIE_FILE="session_cookie.txt"
+if [ ! -f "$COOKIE_FILE" ]; then
+    echo "Error: Session cookie file not found ($COOKIE_FILE)."
     exit 1
 fi
 
+AOC_SESSION_COOKIE=$(cat "$COOKIE_FILE")
+
 # Create the directory for the day
-DAY_DIR="day$(printf "%02d" $DAY)"
-mkdir -p $DAY_DIR
+DAY_DIR="day$(printf "%02d" "$DAY")"
+mkdir -p "$DAY_DIR"
 
 # Check if input.txt already exists
 if [ -f "$DAY_DIR/input.txt" ]; then
@@ -27,7 +35,10 @@ else
     # Fetch input from Advent of Code and save it to input.txt
     INPUT_URL="https://adventofcode.com/$YEAR/day/$DAY/input"
 
-    curl "$INPUT_URL" -H "cookie: session=$AOC_SESSION_COOKIE" --user-agent "User-Agent: github.com/kfl/adventofcode_utils/blob/main/aoc_input.sh by ken@friislarsen.net" -o "$DAY_DIR/input.txt"
+    curl "$INPUT_URL" \
+         --cookie "session=$AOC_SESSION_COOKIE" \
+         --user-agent "$USER_AGENT" \
+         -o "$DAY_DIR/input.txt"
 
     echo "Input fetched and saved for day $DAY."
 fi

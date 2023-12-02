9ea5a850ceb42b489e2131999d198fc14c874a64
Ken Friis Larsen <ken@friislarsen.net>
2023-12-02 16:20:34 +0100

    Update Haskell template

--- haskell/DAY.hs
+++ haskell/DAY.hs
@@ -1,22 +1,27 @@
 {-# LANGUAGE LambdaCase, Strict #-}
 module Main where
 
+import qualified Data.Char as C
 import qualified Data.List as L
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map)
 
 test =  map parse []
 input = map parse . lines <$> readFile "input.txt"
 
--- parse str = undefined
+type Input = [Int]
 
--- part1, part2 :: Input -> Int
+parse str = undefined
 
+-- part1 :: Input -> Int
 -- part1 input = undefined
 -- answer1 = part1 <$> input
 
+-- part2 :: Input -> Int
 -- part2 input = undefined
 -- answer2 = part2 <$> input
 
--- main = undefined -- do
+main = undefined -- do
   -- inp <- input
   -- print $ part1 inp
   -- print $ part2 inp
